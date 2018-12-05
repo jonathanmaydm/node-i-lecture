9a30268fbe6e5463f029fb3d1356479d05f333cc
Jonathan May <jonathan.may@devmountain.com>
2018-12-05 12:57:01 -0600

    Add comments

--- controller.js
+++ controller.js
@@ -1,13 +1,35 @@
 const { students } = require("./students");
 
+/**
+ * All express handler functions have three
+ * parameters - request, response, and next -
+ * often shortened to req, res, next.
+ */
+
 const getStudents = (req, res, next) => {
+  /**
+   * res allows us to build or modify the
+   * response that is sent back to the client.
+   * res.json() ensures that the data is sent
+   * as a json object.
+   */
   res.json(students);
 };
 
 const getStudent = (req, res, next) => {
+  /**
+   * Url parameters are available as properties
+   * on the req.params object.
+   */
   const id = req.params.id;
   const student = students.find(student => +student.id === +id);
   if (!student) {
+    /**
+     * The default status for a response is 200 - OK.
+     * We can change this by using res.status() and passing
+     * in the status code we want to use. We still need
+     * to use .json afterwards to actually send data.
+     */
     res.status(400).json({ error: "No student found. :(" });
   } else {
     res.json(student);
@@ -15,6 +37,12 @@ const getStudent = (req, res, next) => {
 };
 
 const addStudent = (req, res, next) => {
+  /**
+   * Inside of our handler functions, we just use
+   * regular Javascript. You can console.log
+   * any value and it will display in your terminal
+   * where you are running node or nodemon.
+   */
   req.body.id = students.length;
   students.push(req.body);
   res.json(students);
@@ -33,6 +61,13 @@ const updateStudent = (req, res, next) => {
   res.json(students);
 };
 
+/**
+ * module.exports is the Node way
+ * of exporting code to be used
+ * in another file. In this case, it
+ * is an object whose values are all
+ * functions.
+ */
 module.exports = {
   getStudents,
   getStudent,
--- index.js
+++ index.js
@@ -1,10 +1,60 @@
+/**
+ * express is a library that makes it
+ * easier to create HTTP servers
+ *
+ * body-parser is a library that makes
+ * the body (or data) of a request
+ * available to us as req.body
+ * */
+
 const express = require("express");
 const { json } = require("body-parser");
-const controller = require("./controller");
-const app = express();
-const PORT = 5050;
 
+/**
+ * Using a controller allows us to clean
+ * up our index.js so it is easier to read
+ * and reason about. The controller holds
+ * the implementation details (or callback
+ * functions) for how express should handle
+ * each request.
+ */
+const controller = require("./controller");
+
+/**
+ * Invoking express creates an express app
+ * for us, which provides convenience methods
+ * like .get(), .post(), .put(), and .delete()
+ */
+const app = express();
+
+/**
+ * app.use allows for top-level middleware,
+ * or an additional function that runs before
+ * our handlers for every single request.
+ *
+ * json() comes from body-parser and converts
+ * the body (or data) sent in a request to a
+ * Javascript object and makes it available
+ * to us on req.body
+ */
 app.use(json());
+
+/**
+ * Our express app has convenience methods for
+ * each HTTP verb (get, post, put, delete). They
+ * take in two parameters, a path and a callback
+ * function. The callback function is invoked if
+ * and only if the path parameter matches the path
+ * of the request and uses the correct HTTP verb.
+ *
+ * Path parameters have a : before the parameter name.
+ * These path parameters can be accessed in the
+ * callback function as keys of the req.params object.
+ * The values are always strings.
+ *
+ * Ex. With a path of "/api/students/:id"
+ * req.params is { id: 'some string value' }
+ */
 
 app.get("/api/students", controller.getStudents);
 app.get("/api/students/:id", controller.getStudent);
@@ -12,6 +62,15 @@ app.post("/api/students", controller.addStudent);
 app.delete("/api/students/:id", controller.deleteStudent);
 app.put("/api/students/:id", controller.updateStudent);
 
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+/**
+ * app.listen tells express to always be listening
+ * on the port passed in as the first argument.
+ * The second argument is a callback function that
+ * runs once the server is listening. It often has a
+ * simple message indicating the port that is being
+ * listened to.
+ */
+
+app.listen(5050, () => {
+  console.log("Listening on 5050");
 });
